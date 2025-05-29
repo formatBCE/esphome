94a53badcc68de88a91ed1f90b57157293bb9e90
Andrii Mitnovych <10116550+formatBCE@users.noreply.github.com>
2025-05-29 07:16:55 -0700

    Disable VARIANT_ESP32P4 (conflicts with 2025.5.1)

--- esphome/components/i2s_audio/__init__.py
+++ esphome/components/i2s_audio/__init__.py
@@ -4,7 +4,7 @@ from esphome.components.esp32 import get_esp32_variant
 from esphome.components.esp32.const import (
     VARIANT_ESP32,
     VARIANT_ESP32C3,
-    VARIANT_ESP32P4,
+    #VARIANT_ESP32P4, // conflicts with 2025.5.1
     VARIANT_ESP32S2,
     VARIANT_ESP32S3,
 )
@@ -75,7 +75,7 @@ I2S_PORTS = {
     VARIANT_ESP32S2: 1,
     VARIANT_ESP32S3: 2,
     VARIANT_ESP32C3: 1,
-    VARIANT_ESP32P4: 3,
+    #VARIANT_ESP32P4: 3, // conflicts with 2025.5.1
 }
 
 i2s_channel_fmt_t = cg.global_ns.enum("i2s_channel_fmt_t")
